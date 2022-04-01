5985426ddb9412131dcd68038a103f2d02b600ff
Adam Blanchet <ab59@sanger.ac.uk>
2022-04-01 11:07:23 +0000

    Add copyright statements.
    Signed-off-by: Adam Blanchet <ab59@sanger.ac.uk>

--- transponster/util.py
+++ transponster/util.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2022 Genome Research Ltd. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @author Adam Blanchet <ab59@sanger.ac.uk>
+
 """Useful types"""
 from dataclasses import dataclass
 from enum import Enum, auto
@@ -90,7 +109,7 @@ class Script:
         working_directory = Path(working_dir)
         input_folder = Path(working_dir, "input")
         LOGGER.info(f"run script in directory {working_directory}")
-        
+
         process = subprocess.run(
             [self.path, input_folder],
             cwd=working_directory,
