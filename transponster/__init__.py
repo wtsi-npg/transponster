b9118bceebc59509c138aa26392c2fc259a28ad5
Adam Blanchet <ab59@sanger.ac.uk>
2022-08-17 08:30:58 +0000

    Add copyright boilerplate to all files.
    Signed-off-by: Adam Blanchet <ab59@sanger.ac.uk>

--- transponster/__init__.py
+++ transponster/__init__.py
@@ -1,22 +1,21 @@
-# -*- coding: utf-8 -*-
+# Copyright (c) 2022 Genome Research Ltd.
 #
-# Copyright © 2022 Genome Research Ltd. All rights reserved.
+# Author: Adam Blanchet <ab59@sanger.ac.uk>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This file is part of transponster.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# transponster is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
 #
-# @author Adam Blanchet <ab59@sanger.ac.uk>
-
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
 """Transponster.
 
 Get files from iRODS, process them, and upload the results.
